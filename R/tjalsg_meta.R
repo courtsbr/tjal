765a2b4b38f7d51872d57d054dad5a45cc2d7f70
jjesusfilho <jjesusfilho@gmail.com>
2017-05-29 19:35:14 -0300

    Ajustes

--- R/tjalsg_meta.R
+++ R/tjalsg_meta.R
@@ -7,14 +7,14 @@
 #' @param inicio  Data inicial
 #' @param fim  Data final
 #' @keywords tjal, judicial decisions, high court, alagoas
-#' @import XML
+#' @import xml2
 #' @import httr
 #' @import stringr
 #' @export
 #' @examples
 #' tjalsg_meta(livre="Lei Maria da Penha")
 
-tjalsg_meta<-function(livre,quote=TRUE,classes.value="",inicio="",fim=""){
+tjalsg_meta<-function(BuscaLivre,quote=TRUE,classes.value="",inicio="",fim=""){
   set_config(config(ssl_verifypeer = FALSE ))
   body <- list(dados.buscaInteiroTeor ="", dados.pesquisarComSinonimos = "S",
                dados.pesquisarComSinonimos = "S", dados.buscaEmenta = "",
@@ -34,32 +34,40 @@ tjalsg_meta<-function(livre,quote=TRUE,classes.value="",inicio="",fim=""){
                #tipoDecisaoSelecionados = "", tipoDecisaoSelecionados = "",
                dados.ordenacao = "data")
   if(quote==TRUE) livre<-deparse(livre)
-  body[[1]]<-livre
+  body[[1]]<-BuscaLivre
   body[[19]]<-classes.value ##
   body[[30]]<-inicio ## colocar a data no formato dd/mm/aaa
   body[[31]]<-fim # idem
   a<-POST("http://www2.tjal.jus.br/cjsg/resultadoCompleta.do",encode="form",
           body=body)
-  b<- htmlParse(content(a,as="text"), encoding = "UTF-8")
-  val <- xmlGetAttr(getNodeSet(b, "//*[@id='totalResultadoAba-A']")[[1]],"value")
-  num<-as.numeric(val)
-  max_pag <- ceiling(num/15)
+  b<- content(a,as="parsed")
+  max_pag<-b %>% xml_find_all(xpath="//*[@id='totalResultadoAba-A']") %>%
+    xml_attr(attr="value") %>%
+    as.numeric() %>%
+    `/`(15) %>%
+    ceiling()
+
   df<-data.frame()
   for (i in 1:max_pag){
     tryCatch({
       c <- GET(paste0("http://www2.tjal.jus.br/cjsg/trocaDePagina.do?tipoDeDecisao=A&pagina=",i), set_cookies(unlist(a$cookies)))
-      d <- htmlParse(content(c,as="text"), encoding = "UTF-8")
-      aC <-xpathSApply(d,'//*[@class="assuntoClasse"]',xmlValue,trim=T)
+      d <- content(c,as="parsed")
+      aC <-d %>%
+        xml_find_all('//*[@class="assuntoClasse"]') %>%
+        xml_text(trim=T)
       assunto.processo<-str_trim(str_extract(aC,".*?(?=/)"))
       assunto.materia<-str_trim(str_extract(aC,"(?<=/\\s{0,100}).*"))
-      relator<-xpathSApply(d,'//*[@class="ementaClass2"][1]',xmlValue,trim=T)
-      comarca<-xpathSApply(d,'//*[@class="ementaClass2"][2]',xmlValue,trim=T)
-      orgao.julgador<-xpathSApply(d,'//*[@class="ementaClass2"][3]',xmlValue,trim=T)
-      data.julgamento<-xpathSApply(d,'//*[@class="ementaClass2"][4]',xmlValue,trim=T)
-      data.registro<-xpathSApply(d,'//*[@class="ementaClass2"][5]',xmlValue,trim=T)
-      ementa<-xpathSApply(d,"//*[@class='ementaClass2'][6]",xmlValue,trim=T)
-      processo<-xpathSApply(d,'//*[@class="esajLinkLogin downloadEmenta"]',xmlValue,trim=T)
-      cdacordao<-xpathSApply(d,'//*[@class="downloadEmenta"]',xmlAttrs,trim=T)[2,]
+      meta<-xml_find_all(d,xpath="//*[@valign='top'][2]") %>% xml_text(trim=T)
+      meta<-str_replace_all(meta,"\\s{2,}"," ")
+      relator<-str_extract(meta,"Relator.*?(?=\\s(Comarca|Data|Ementa))")
+      comarca<-str_extract(meta,"Comarca.*?(?=\\s(Data|Ementa))")
+      orgao.julgador<-str_extract(meta,"Órgão.*?(?=\\s(Data|Ementa))")
+      data.julgamento<-str_extract(meta,"Data\\sdo.*?(?=\\s(Data|Ementa))")
+      data.registro<-str_extract(meta,"Data\\sde.*?(?=\\s(Data|Ementa))")
+      ementa<-str_extract(meta,"Ementa.*")
+      processo<-str_extract(meta,"\\d+.*?(?=\\s)")
+      cdacordao<-xml_find_all(d,xpath="//@cdacordao") %>% xml_text(trim=T)
+
       df1<-data.frame(pagina=i,assunto.processo,assunto.materia,relator,comarca,orgao.julgador,data.julgamento,data.registro,processo,ementa,cdacordao,stringsAsFactors = F)
       df<-rbind(df,df1)
     },
@@ -73,7 +81,7 @@ tjalsg_meta<-function(livre,quote=TRUE,classes.value="",inicio="",fim=""){
   }
   df[4:8]<-lapply(df[4:8],function(x) str_replace(x,".*:\\s*",""))
   df$url<-paste0("http://www2.tjal.jus.br/cjsg/getArquivo.do?cdAcordao=",df$cdacordao,"&cdForo=0")
-  
+
   return(df)
 }
 
